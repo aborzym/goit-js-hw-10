56de724e0ababa1e9f08537827ac956fcb813e16
aborzym <borzym.a@gmail.com>
2024-04-21 19:27:42 +0200

    03

--- src/index.js
+++ src/index.js
@@ -22,13 +22,34 @@ fetchBreeds()
 	.finally();
 
 select.addEventListener('change', (ev) => {
-	console.log(ev.target.value);
-});
-//te poniżej są, żeby sprawdzić, czy działa
-getCatPhotoByBreed('awir').then((data) => {
-	console.log(data[0].url);
+	let catData = {};
+	const breedID = ev.target.value;
+	getCatPhotoByBreed(breedID)
+		.then((photo) => {
+			catData.url = photo[0].url;
+		})
+		.catch();
+	fetchCatByBreed(breedID)
+		.then((catDescription) => {
+			const { name, description, temperament } = catDescription;
+			catData.name = name;
+			catData.description = description;
+			catData.temperament = temperament;
+		})
+		.catch();
 });
 
-fetchCatByBreed('awir').then((cat) => {
-	console.log(cat.name);
-});
+// innerHTML:
+
+// `<img
+// 				class="cat-info__photo"
+// 				src="${catData.url}"
+// 				alt="cat image"
+// 			/><div class="cat-info__description">
+// 				<h2 class="cat-name">${catData.name}</h2>
+// 				<p class="cat-description">
+// 					${catData.description}
+// 				</p>
+// 				<p class="cat-temperament">
+// 					<span class="cat-temperament__name">Temperament: </span>${catData.description}
+// 				</p>`;
