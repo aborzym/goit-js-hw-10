1816622023772b84b5dee2fbd4ae4548da6b07c5
aborzym <borzym.a@gmail.com>
2024-04-21 16:33:09 +0200

    01

---
+++ src/cat-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+//Napisz funkcję fetchBreeds(), która wysyła żądanie HTTP i zwraca obietnicę z tablicą ras - wynikiem żądania. Umieść ją w pliku cat-api.js i dokonaj nazwanego eksportu.
+
+export function fetchBreeds() {
+	return axios.get('https://api.thecatapi.com/v1/breeds').then((breeds) => {
+		return breeds.data;
+	});
+}
+
+//Napisz funkcję fetchCatByBreed(breedId), która oczekuje identyfikatora rasy, wykonuje żądanie HTTP i zwraca obietnicę z danymi o kocie - wynikiem żądania. Umieść ją w pliku cat-api.js i dokonaj nazwanego eksportu.
+
+export function fetchCatByBreed(breedId) {
+	return axios.get('https://api.thecatapi.com/v1/breeds').then((breeds) => {
+		return breeds.data.find((el) => el.id === breedId);
+	});
+}
+
+export function getCatPhotoByBreed(breedId) {
+	return axios
+		.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+		.then((info) => {
+			return info.data;
+		});
+}
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { fetchBreeds, fetchCatByBreed, getCatPhotoByBreed } from './cat-api';
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+	'live_i91leozVNIE98NugOCrDU2RPcZl4xEUJguzWlynsfs5mQeJI4TAskccLiIN1comy';
+
+const select = document.querySelector('select.breed-select');
+const loader = document.querySelector('p.loader');
+const error = document.querySelector('p.error');
+const display = document.querySelector('div.cat-info');
+select.classList.add('hidden');
+error.classList.add('hidden');
+
+fetchBreeds()
+	.then((data) => {
+		select.innerHTML = data.map(
+			(item) => `<option value="${item.id}">${item.name}</option>`
+		);
+		select.classList.remove('hidden');
+		loader.classList.add('hidden');
+	})
+	.catch()
+	.finally();
+
+select.addEventListener('change', (ev) => {
+	console.log(ev.target.value);
+});
+getCatPhotoByBreed('awir').then((data) => {
+	console.log(data[0].url);
+});
+
+fetchCatByBreed('awir').then((cat) => {
+	console.log(cat.name);
+});
